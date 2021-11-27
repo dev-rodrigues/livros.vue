741186fa5e0d126fbe937666dc270b1a6f2f8cd1
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-11-27 19:46:19 -0300

    exibindo lista de livros

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,13 +1,37 @@
 <template>
-    <h1>livros</h1>    
+    <div>
+        <div>
+            <h1>Books</h1>
+            <div class="books">
+                <div
+                    v-for="(book, index) in allBooks"
+                    class="book"
+                    v-bind:key="index">
+                    {{ book.title }}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import { mapGetters, mapActions } from 'vuex';
     export default {
-        name: "Books"
+        name: "Books",
+        methods: {
+            ...mapActions(['fetchBooks'])
+        },
+        computed: mapGetters(['allBooks']),
+        created() {
+            this.fetchBooks();
+        }
     }
 </script>
 
 <style scoped>
-
+    .books {
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        grid-gap: 1rem;
+    }    
 </style>
