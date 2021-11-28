6bb1a01af0313ba6fc63abafd20e12b25cc4285e
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-11-28 15:24:32 -0300

    adiciona css perdido durante instalacao de vue router

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,44 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+  }
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  :root {
+      --denial: #f21d41;
+      --completed: #f38264;
+      --allowed: #75baa8;
+      --text: #0f0f0f;      
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: var(--text);
+  }  
+  .root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+  }
+  .container {
+      max-width: 10240px;
+      display: flex;
+  }
 </style>
